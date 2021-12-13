fe36b87966e78af0adff12e5cfb34114e1a0905c
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2021-12-12 22:12:11 -0500

    Update DataPrepLoop.py

--- DataPrepLoop.py
+++ DataPrepLoop.py
@@ -6,7 +6,7 @@ import os
 
 invalid_files = [216676]
 
-for k in range(216654,216814,2): 
+for k in range(219502,219620,2): 
     print(k)
     if k in invalid_files:
         print('not including')
