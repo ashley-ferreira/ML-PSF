2b8334d75fd4a86733d43340b3b60572ebd3f02f
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-04-26 10:12:01 -0500

    Update Star_NN_valid.py

--- DevelopmentCode/CNN/Star_NN_valid.py
+++ DevelopmentCode/CNN/Star_NN_valid.py
@@ -164,7 +164,7 @@ def validate_CNN(model_dir_name, data):
     # load model                         
     model_found = False 
     for file in os.listdir(model_dir_name+'10epochs_basic_model'):
-        if file.startswith('model_'):
+        if file.startswith('model_350'):
             cn_model = keras.models.load_model(model_dir_name + '10epochs_basic_model/model_350')#file)
             print('using model:', file)
             model_found = True
