5cd6ee71fad2fe659441d040490837376441174d
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-04-27 13:06:04 -0400

    Update Star_NN_dev.py

--- DevelopmentCode/CNN/Star_NN_dev.py
+++ DevelopmentCode/CNN/Star_NN_dev.py
@@ -427,7 +427,7 @@ def train_CNN(model_dir_name, num_epochs, data):
         '''
         def on_epoch_end(self, epoch, logs={}):
             if epoch % 10 == 0 and epoch != 0:
-                self.model.save(model_dir_name + 'models_each_10epochs/' + "model_{}".format(epoch))
+                self.model.save(model_dir_name + 'models_each_10epochs_alt/' + "model_{}".format(epoch))
 
     # unpack presaved data
     cutouts, labels, xs, ys, fwhms, files = data[0], data[1], data[2], data[3], data[4], data[5]
