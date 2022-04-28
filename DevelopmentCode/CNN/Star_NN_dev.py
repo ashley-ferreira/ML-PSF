b536aaa4ed24ea574c9657a2f909c2ad06ed9ffa
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-04-28 09:54:00 -0400

    Update Star_NN_dev.py

--- DevelopmentCode/CNN/Star_NN_dev.py
+++ DevelopmentCode/CNN/Star_NN_dev.py
@@ -436,7 +436,7 @@ def train_CNN(model_dir_name, num_epochs, data):
     batch_size = 256 # up from 16 --> 1024 --> 32 --> 256
     dropout_rate = 0.2
     test_fraction = 0.05 
-    learning_rate = 0.00001 # down from 0.001 --> 0.0005
+    learning_rate = 0.001 # down from 0.001 --> 0.0005 --> 0.00001
 
     ### now divide the cutouts array into training and testing datasets.
     skf = StratifiedShuffleSplit(n_splits=1, test_size=test_fraction, random_state=0)
