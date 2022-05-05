e15fd1a810c3ae82a6b8f0136a6ec4bae5caad7a
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-05-04 23:24:52 -0400

    Update Star_NN_dev_otherM.py

--- DevelopmentCode/CNN/Star_NN_dev_otherM.py
+++ DevelopmentCode/CNN/Star_NN_dev_otherM.py
@@ -422,7 +422,7 @@ def train_CNN(model_dir_name, num_epochs, data):
         y_test (arr): real y values (labels) for testing 
 
     '''
-    sub_mod_dir = model_dir_name + 'models_basic2048_lr=0.001/'
+    sub_mod_dir = model_dir_name + 'models_basic256_lr=0.001_drop=0.4_split=0.4/'
     if not(os.path.exists(sub_mod_dir)):
         os.mkdir(sub_mod_dir)
     class CustomSaver(keras.callbacks.Callback):
@@ -438,9 +438,9 @@ def train_CNN(model_dir_name, num_epochs, data):
     cutouts, labels, xs, ys, fwhms, files = data[0], data[1], data[2], data[3], data[4], data[5]
 
     # section for setting up some flags and hyperparameters
-    batch_size = 1024*2 # up from 16 --> 1024 --> 32 --> 256
-    dropout_rate = 0.2
-    test_fraction = 0.2 # from 0.05
+    batch_size = 256 # up from 16 --> 1024 --> 32 --> 256
+    dropout_rate = 0.4
+    test_fraction = 0.4 # from 0.05
     learning_rate = 0.001#01 # down from 0.001 --> 0.0005 --> 0.00001
 
     ### now divide the cutouts array into training and testing datasets.
