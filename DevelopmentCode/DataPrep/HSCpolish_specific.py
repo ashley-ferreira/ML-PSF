fa6d20981864cfcf4937fb5dd2eceb03150a6af1
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-05-11 15:55:50 -0300

    Create HSCpolish_specific.py

---
+++ DevelopmentCode/DataPrep/HSCpolish_specific.py
@@ -0,0 +1,14 @@
+from re import A
+from HSCgetStars_func import HSCgetStars_main 
+from HSCpolishPSF_func import HSCpolishPSF_main
+import os
+import sys
+
+file_in = 'CORR-0219612-007.fits'
+training_dir = '/arc/projects/uvickbos/ML-PSF/NN_data_111/'
+file_dir = '/arc/projects/uvickbos/ML-PSF/home_dir_transfer/HSC_May25-lsst/rerun/processCcdOutputs/03074/HSC-R2/corr/'
+
+cutout_file = training_dir + file_in.replace('.fits', '_' + str(111) 
+                                                                + '_cutouts_savedFits.pickle')
+
+HSCpolishPSF_main(file_dir, file_in, cutout_file, 111, training_dir)
