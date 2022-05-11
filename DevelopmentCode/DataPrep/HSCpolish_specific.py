ea79bd6c306a09b6405788e8e5dc61c7870b93cb
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-05-11 16:12:36 -0300

    Update HSCpolish_specific.py

--- DevelopmentCode/DataPrep/HSCpolish_specific.py
+++ DevelopmentCode/DataPrep/HSCpolish_specific.py
@@ -4,7 +4,7 @@ from HSCpolishPSF_func import HSCpolishPSF_main
 import os
 import sys
 
-file_in = 'CORR-0219612-007.fits'
+file_in = 'CORR-0219612-077.fits'
 training_dir = '/arc/projects/uvickbos/ML-PSF/NN_data_111/'
 file_dir = '/arc/projects/uvickbos/ML-PSF/home_dir_transfer/HSC_May25-lsst/rerun/processCcdOutputs/03074/HSC-R2/corr/'
 
