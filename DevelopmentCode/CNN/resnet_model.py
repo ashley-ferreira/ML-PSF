960092462eee763d011aba568ce434f9105ec4b2
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-05-26 16:15:32 -0300

    Update resnet_model.py

--- DevelopmentCode/CNN/resnet_model.py
+++ DevelopmentCode/CNN/resnet_model.py
@@ -1,40 +1,79 @@
-from tensorflow import Tensor
-from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization,\
-                                    Add, AveragePooling2D, Flatten, Dense
-from tensorflow.keras.models import Model
-def relu_bn(inputs: Tensor) -> Tensor:
-    relu = ReLU()(inputs)
-    bn = BatchNormalization()(relu)
-    return bn
-def residual_block(x: Tensor, filters: int, kernel_size: int = 3) -> Tensor:
-    y = Conv2D(kernel_size=kernel_size,
-               strides= 1,
-               filters=filters,
-               padding="same")(x)
-    y = relu_bn(y)
-    y = Conv2D(kernel_size=kernel_size,
-               strides=1,
-               filters=filters,
-               padding="same")(y)
-    out = Add()([x, y])
-    out = relu_bn(out)
-    return out
-def resnet(input_shape, unique_labs=2, num_filters=32, num_densefilters=32):
-    inputs = Input(shape=input_shape)
-    t = BatchNormalization()(inputs)
-    num_blocks_list = [2, 2, 2]
-    for i in range(len(num_blocks_list)):
-        num_blocks = num_blocks_list[i]
-        for j in range(num_blocks):
-            t = residual_block(t, filters=num_filters)
-        num_filters *= 2
-        t = MaxPool2D(pool_size=(2, 2), padding='valid')(t)
-    t = Flatten()(t)
-    outputs = Dense(num_densefilters, activation='softmax')(t)
-    model = Model(inputs, outputs)
-    model.compile(
-        optimizer='adam',
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
-    )
-    return model
+from keras.models import Model, load_model, Sequential
+from keras.layers import Input, Dense, Activation, Add, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, MaxPool2D, Concatenate
+from keras.layers.core import Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.optimizers import Adam
+
+
+def identity_block(self, x, filter):
+    # copy tensor to variable called x_skip
+    x_skip = x
+    # Layer 1
+    x = Conv2D(filter, (3,3), padding = 'same')(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Activation('relu')(x)
+    # Layer 2
+    x = Conv2D(filter, (3,3), padding = 'same')(x)
+    x = BatchNormalization(axis=-1)(x)
+    # Add Residue
+    x = Add()([x, x_skip])
+    x = Activation('relu')(x)
+    return x
+def convolutional_block(self, x, filter):
+    # copy tensor to variable called x_skip
+    x_skip = x
+    # Layer 1
+    x = Conv2D(filter, (3,3), padding = 'same', strides = (2,2))(x)
+    x = BatchNormalization(axis=-1)(x)
+    x = Activation('relu')(x)
+    # Layer 2
+    x = Conv2D(filter, (3,3), padding = 'same')(x)
+    x = BatchNormalization(axis=-1)(x)
+    # Processing Residue with conv(1,1)
+    x_skip = Conv2D(filter, (1,1), strides = (2,2))(x_skip)
+    # Add Residue
+    x = Add()([x, x_skip])
+    x = Activation('relu')(x)
+    return x
+
+    #how to add thest to self?
+
+def convnet_model_resnet(input_shape, unique_labs=2, dropout_rate=0.2):
+
+    # init function
+    self.input_shape = input_shape
+    self.identity_block(x, filter_size)
+    self.convolutional_block(x, filter_size)
+
+    model = Sequential()
+
+# Step 2 (Initial Conv layer along with maxPool)
+x = Conv2D(filters=16, kernel_size=(3, 3), input_shape=self.input_shape, padding='same')(x)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
+x = MaxPool2D(pool_size=3, strides=2, padding='same')(x)
+# Define size of sub-blocks and initial filter size
+block_layers = [2, 2]#[3, 4, 6, 3]
+filter_size = 16
+# Step 3 Add the Resnet Blocks
+for i in range(len(block_layers)):
+    if i == 0:
+        # For sub-block 1 Residual/Convolutional block not needed
+        for j in range(block_layers[i]):
+            x = self.identity_block(x, filter_size)
+    else:
+        # One Residual/Convolutional Block followed by Identity blocks
+        # The filter size will go on increasing by a factor of 2
+        filter_size = filter_size*2
+        x = self.convolutional_block(x, filter_size)
+        for j in range(block_layers[i] - 1):
+            x = self.identity_block(x, filter_size)
+# Step 4 End Dense Network
+#x = AveragePooling2D((2,2), padding = 'same')(x)
+x = MaxPool2D(pool_size=(2,2), padding = 'same')(x)
+x = Flatten()(x)
+x = Dense(self.num_dense_nodes, activation = self.activation)(x)
+#x = Dense(self.num_dense_nodes, activation = self.activation)(x)
+output = Dense(self.unique_labels, activation = 'softmax')(x)
+
+    return output 
