c315da3580d3e367f3bd95bb85d64151da067f69
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-07-12 15:27:10 -0400

    Update Star_NN_valid_poster.py

--- DevelopmentCode/CNN/Star_NN_valid_poster.py
+++ DevelopmentCode/CNN/Star_NN_valid_poster.py
@@ -307,6 +307,7 @@ def validate_CNN(model_dir_name, data):
     pyl.title('Accuracy Curve & Confidence Histogram')
     bins = np.linspace(0, 1, 100)
     weights = np.ones_like(test_good_p)/len(test_good_p)
+    pyl.vlines(0.5, ymin=0, ymax=1, alpha=0.5, color='purple', label='default 0.5 confidence cutoff')
     pyl.hist(test_good_p, label='normalized confidence histogram', bins=bins, alpha=0.5, weights=weights*2.5)#normed=True)#density=True)
     pyl.plot(confidence_queries, good_star_acc, label='good source classification accuracy', alpha=0.8)
     pyl.xlabel('Good Source Confidence')
