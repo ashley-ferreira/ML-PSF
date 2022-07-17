c27c72ffdbc54105c98f8b181dd243eb17bb7330
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-07-17 12:23:29 -0400

    Update plotting_timing.py

--- DevelopmentCode/CreatePSF/plotting_timing.py
+++ DevelopmentCode/CreatePSF/plotting_timing.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 
 names = ['216652-000', '216652-001']
-ml_preds = [0.524, 0.641]
-time_ml = [0.602, 0.7449]
-time_nonml = [60.366]
+ml_preds = [0.5049, 0.627114]
+time_ml = [0.5796, 0.72742]
+time_nonml = [61.096, 118.0005]
 # log axis?
 
 #plt.plot(time_ml, names, 'o', alpha = 0.5, color='b') # rotate names and put on x?
@@ -19,12 +19,13 @@ x = np.arange(len(names))
 width = 0.2
   
 # plot data in grouped manner of bar type
-plt.bar(x-0.2, ml_preds, width, color='cyan')
-plt.bar(x, time_ml, width, color='orange')
-plt.bar(x+0.2, time_nonml, width, color='green')
+plt.bar(x-0.2, ml_preds, width, alpha=0.5)
+plt.bar(x, time_ml, width, alpha=0.7)
+plt.bar(x+0.2, time_nonml, width, color='purple', alpha=0.7)
+plt.yscale('log')
 plt.xticks(x, names)
 plt.title("Source Selection Time Comparison")
 plt.xlabel("image name (may 26, 2020)")
 plt.ylabel("time (seconds)")
-plt.legend(["ml time (just prediction)", "ml time", "non-ml time"])
+plt.legend(["ml prediction only time", "ml process time", "non-ml proccess time"])
 plt.show()
