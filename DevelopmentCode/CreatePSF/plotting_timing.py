48880e75623ddc23d8cc4e70a391e41e4bd26bd2
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-07-17 13:36:53 -0400

    Update plotting_timing.py

--- DevelopmentCode/CreatePSF/plotting_timing.py
+++ DevelopmentCode/CreatePSF/plotting_timing.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 # 0.9 conf cutoff
 
-names = ['216652-000', '216652-001', '216652-002', '216652-003', '216652-004', '216652-005']
-ml_preds = [0.5049, 0.627114, 0.6088, 0.6094, 0.6114, 0.6723]
-time_ml = [0.5796, 0.72742, 0.71030, 0.7161, 0.703265, 0.777109]
-time_nonml = [61.096, 118.0005, 133.888, 158.418, 85.749, 177.521]
+names = ['216652-000', '216652-001', '216652-002', '216652-003', '216652-004', '216652-005', '216652-005', '216652-006']
+ml_preds = [0.5049, 0.627114, 0.6088, 0.6094, 0.6114, 0.6723, 0.627]
+time_ml = [0.5796, 0.72742, 0.71030, 0.7161, 0.703265, 0.777109, 0.827]
+time_nonml = [61.096, 118.0005, 133.888, 158.418, 85.749, 177.521, 148.296]
 # log axis?
 
 #plt.plot(time_ml, names, 'o', alpha = 0.5, color='b') # rotate names and put on x?
@@ -31,5 +31,5 @@ plt.xticks(x, names)
 plt.title("Source Selection Time Comparison")
 plt.xlabel("image name (may 26, 2020)")
 plt.ylabel("time (seconds)")
-plt.legend(["ml prediction only time", "ml process time", "non-ml proccess time"], loc='upper left')
+plt.legend(["CNN model prediction only time", "CNN model process time", "non-CNN proccess time"], loc='upper left')
 plt.show()
